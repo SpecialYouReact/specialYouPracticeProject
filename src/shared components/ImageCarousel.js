e0247145c62919338c5cb6a43cdf566c8a9b759f
Akanksha Khandelwal <akanksha@growpital.com>
2023-03-14 23:55:30 +0530

    error

--- src/shared components/ImageCarousel.js
+++ src/shared components/ImageCarousel.js
@@ -4,8 +4,7 @@ import MyCard from "./MyCard";
 import React from "react";
 
 const ImageCarousel = () => {
-  let box = document.querySelector(".product-container");
-
+  const box = document.querySelector('.product-container');
   const previousButtonHandler = () => {
     let width = box.clientWidth;
     box.scrollLeft = box.scrollLeft - width;
@@ -16,30 +15,30 @@ const ImageCarousel = () => {
   };
 
   return (
-    <div className="product-carousel">
-      <button className="previousButton" onClick={previousButtonHandler}>
+    <div className='product-carousel'>
+      <button className='previousButton' onClick={previousButtonHandler}>
         <p>&lt;</p>
       </button>
-      <button className="nextButton" onClick={nextButtonHandler}>
+      <button className='nextButton' onClick={nextButtonHandler}>
         <p>&gt;</p>
       </button>
-      <div className="product-container">
-        <MyCard cardno="1" />
-        <MyCard cardno="2" />
-        <MyCard cardno="3" />
-        <MyCard cardno="4" />
-        <MyCard cardno="5" />
-        <MyCard cardno="6" />
-        <MyCard cardno="7" />
-        <MyCard cardno="8" />
-        <MyCard cardno="9" />
-        <MyCard cardno="10" />
-        <MyCard cardno="11" />
-        <MyCard cardno="12" />
-        <MyCard cardno="13" />
-        <MyCard cardno="14" />
-        <MyCard cardno="15" />
-        <MyCard cardno="16" />
+      <div className='product-container'>
+        <MyCard cardno='1' />
+        <MyCard cardno='2' />
+        <MyCard cardno='3' />
+        <MyCard cardno='4' />
+        <MyCard cardno='5' />
+        <MyCard cardno='6' />
+        <MyCard cardno='7' />
+        <MyCard cardno='8' />
+        <MyCard cardno='9' />
+        <MyCard cardno='10' />
+        <MyCard cardno='11' />
+        <MyCard cardno='12' />
+        <MyCard cardno='13' />
+        <MyCard cardno='14' />
+        <MyCard cardno='15' />
+        <MyCard cardno='16' />
       </div>
     </div>
   );
