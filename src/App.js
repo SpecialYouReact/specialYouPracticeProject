be2d038b3fbd6d7d02568a36f8656c11638cf608
SpecialYouReact <specialyoutech@gmail.com>
2023-03-14 15:12:57 +0530

    casual commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import "./App.css";
-import Button from "./shared components/button/Button";
-// import { Button } from "@mui/material";
-// import other from "./shared assets/icons/index";
-import { HeartIcon } from "./shared assets/icons";
-import Nature from "./shared assets/images/nature.jpg";
-import specialYou from "./shared assets/images/1 - TB-JPZR-L64L/1.jpg";
+// import Button from "./shared components/button/Button";
+// import { HeartIcon } from "./shared assets/icons";
+// import Nature from "./shared assets/images/nature.jpg";
+// import specialYou from "./shared assets/images/1 - TB-JPZR-L64L/1.jpg";
 
 function App() {
-  // console.log(other);
   return (
-    <>
-      <Button />
-      <HeartIcon />
-
-      {/* <Nature /> */}
-      <img src={Nature} height="300px" width="300px" alt="nature" />
-      <img src={specialYou} alt="special" />
-    </>
+    <div>
+      raghav khnadelwal
+      {/* <Button />
+      <HeartIcon /> */}
+    </div>
   );
 }
 
